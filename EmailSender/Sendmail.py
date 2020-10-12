4b8a5975f0b418c5379922db7808856f52e37757
320088280 <320088280@INGBTCPIC5NB405>
2020-10-12 12:28:21 +0530

    Modified Sendmail.py file to make it readable

--- EmailSender/Sendmail.py
+++ EmailSender/Sendmail.py
@@ -1,14 +1,18 @@
 import smtplib 
 
+senderMailId = "prajju1998gaargesh@gmail.com"
+senderMailpassword = "phm@1998"
+receiverMailId = "prajwalgaargesh@gmail.com"
+Subject = "New Image Alert!"
+
 def sendMail(msg):
     email = smtplib.SMTP('smtp.gmail.com', 587) 
     email.starttls() 
+
+    email.login(senderMailId,senderMailpassword) 
     
-    email.login("prajju1998gaargesh@gmail.com", "phm@1998") 
-    
-    Subject = "New Image Alert!"
     Body = msg
     message = 'Subject: {}\n\n{}'.format(Subject, Body)
     
-    email.sendmail("prajju1998gaargesh@gmail.com", "prajwalgaargesh@gmail.com", message) 
+    email.sendmail(senderMailId, receiverMailId, message) 
     email.quit()
