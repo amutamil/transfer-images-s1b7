09cc5692ad268a67d64e0b41c862bdbe2199e551
Kelkar <Dishant.Kelkar@philips.com>
2020-10-14 20:09:59 +0530

    Update general_util.h

--- Sender/general_util.h
+++ Sender/general_util.h
@@ -7,3 +7,8 @@
 void PrintError(std::string A_string, MC_STATUS A_status);
 void PrintStatus(std::string A_string, MC_STATUS A_status);
 void PrintStatus(std::string A_string, bool statusFlag);
+
+SAMP_BOOLEAN TestCmdLine(int A_argc, char* A_argv[], STORAGE_OPTIONS* A_options);
+
+SAMP_BOOLEAN ReadResponseMessages(STORAGE_OPTIONS*  A_options, int A_associationID, int A_timeout, InstanceNode** A_list, InstanceNode* A_node);
+SAMP_BOOLEAN CheckResponseMessage(int A_responseMsgID, unsigned int* A_status, char* A_statusMeaning, size_t A_statusMeaningLength);
